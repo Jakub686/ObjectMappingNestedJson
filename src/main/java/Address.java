9e8392828fd56e41d5215aacacb0689788e0b1b4
Jakub <jakub.bchyla@gmail.com>
2022-05-28 09:32:25 +0200

    working json nested

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,20 +1,20 @@
 public class Address {
-    String street;
     String city;
+    Street street;
 
-    public String getStreet() {
-        return street;
-    }
-    public void setStreet(String street) {
-        this.street = street;
-    }
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
 
-    public Address() {
+    public Street getStreet() {
+        return street;
+    }
+
+    public void setStreet(Street street) {
+        this.street = street;
     }
 }
---
+++ src/main/java/Street.java
@@ -0,0 +1,20 @@
+public class Street {
+    String block;
+    String apartment;
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+}
